f5a2bd352482c5b15ce3e077afc6541703db075e
Dobos Zoltán <113709336+Pedro66Z@users.noreply.github.com>
2022-12-10 23:32:35 +0100

    Add files via upload

---
+++ jegyzet.py
@@ -0,0 +1,21 @@
+print("alma")
+print ("24")
+ez_egy_hosszabb_szam = 7
+print (ez_egy_hosszabb_szam)
+tizedes_tort = 3.14
+print(tizedes_tort)
+
+tree = '120 in garden'
+print (tree)
+
+
+
+oszthatoe = True
+szoveg ="xdxdxdxd"
+szam = 77
+tizedes_tort =33.89
+
+print(type(szoveg))
+print(type(szam))
+print(type(tizedes_tort))
+print(type(oszthatoe))
